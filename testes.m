637cbf865298c01c6ec26e9f9cd773ab3881c33d
Ricardo Pires <ricardo_pires2@live.com.pt>
2018-04-13 21:11:36 +0100

    testes

---
+++ testes.m
@@ -0,0 +1,38 @@
+[x, y, z, a, b, c] = direct_kinematics(0, 0, 0, 0, 0, 0);
+fprintf('direct_kinematics(0, 0, 0, 0, 0, 0) = %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f\n', x, y, z, a, b, c)
+[x, y, z, a, b, c] = direct_kinematics(pi, pi/4, 0, pi/4, pi, pi/2);
+fprintf('direct_kinematics(pi, pi/4, 0, pi/4, pi, pi/2) = %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f\n', x, y, z, a, b, c)
+[x, y, z, a, b, c] = direct_kinematics(pi/2, pi/2, pi/2, pi/2, pi/2, pi/2);
+fprintf('direct_kinematics(pi/2, pi/2, pi/2, pi/2, pi/2, pi/2) = %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f\n', x, y, z, a, b, c)
+[x, y, z, a, b, c] = direct_kinematics(0, pi/2, 0, 0, pi/2, pi/2);
+fprintf('direct_kinematics(0, pi/2, 0, 0, pi/2, pi/2) = %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f\n', x, y, z, a, b, c)
+[x, y, z, a, b, c] = direct_kinematics(0, pi/2, 0, 0, pi/3, 0);
+fprintf('direct_kinematics(0, pi/2, 0, 0, pi/3, 0) = %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f\n', x, y, z, a, b, c)
+[x, y, z, a, b, c] = direct_kinematics(pi, pi/2, 0, 0, pi/2, pi/2);
+fprintf('direct_kinematics(pi, pi/2, 0, 0, pi/2, pi/2) = %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f\n', x, y, z, a, b, c)
+[x, y, z, a, b, c] = direct_kinematics(pi, 0, pi/2, pi/2, 0, pi);
+fprintf('direct_kinematics(pi, 0, pi/2, pi/2, 0, pi) = %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f\n', x, y, z, a, b, c)
+[x, y, z, a, b, c] = direct_kinematics(0, 0, 0, pi/2, pi/2, 0);
+fprintf('direct_kinematics(0, 0, 0, pi/2, pi/2, 0) = %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f\n', x, y, z, a, b, c)
+[x, y, z, a, b, c] = direct_kinematics(pi, pi, pi, pi, pi, pi);
+fprintf('direct_kinematics(pi, pi, pi, pi, pi, pi) = %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f\n', x, y, z, a, b, c)
+[x, y, z, a, b, c] = direct_kinematics(pi, pi/4, pi/2, pi/4, pi/4, 0);
+fprintf('direct_kinematics(pi, pi/4, pi/2, pi/4, pi/4, 0) = %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f\n', x, y, z, a, b, c)
+[x, y, z, a, b, c] = direct_kinematics(0, pi/4, pi/4, 0, 0, pi);
+fprintf('direct_kinematics(0, pi/4, pi/4, 0, 0, pi) = %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f\n', x, y, z, a, b, c)
+
+
+inverse_kinematics(195, 0, 244, 0, 1.570796, 3.141593)
+inverse_kinematics(-8.787, 0.000, 282.848, 0.000, 2.356, 0.785)
+inverse_kinematics(25.000000, -230.000000, 74.000000, -0.000000, 1.570796, 3.141593)
+inverse_kinematics(-140.000, -0.000, 239.000, -3.142, 1.571, 1.571)
+inverse_kinematics(-136.651, -0.000, 251.500, -3.142, 1.047, 0.000)
+inverse_kinematics(140.000, -0.000, 239.000, -0.000, 1.571, 1.571)
+inverse_kinematics(-5.000, -0.000, 384.000, -1.571, 0.000, 0.000)
+inverse_kinematics(170.000, -25.000, 244.000, -1.571, 1.571, -1.571)
+inverse_kinematics(-145.000, 0.000, 4.000, -0.000, 1.571, 0.000)
+inverse_kinematics(192.864, 12.500, 268.831, 0.530, 1.424, 0.530)
+inverse_kinematics(-79.853, 0.000, 348.853, -0.000, 0.000, 0.000)
+
+inverse_kinematics(0, 0, 200, 0, 0, 0)
+
