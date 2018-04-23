0b24cef1bc942d4c84a9f5f1312eabeb2df27282
leonorsilva <leonor.oliveira.e.silva@tecnico.ulisboa.pt>
2018-04-23 15:42:21 +0100

    Add files via upload

--- lab2.m
+++ lab2.m
@@ -1,6 +1,6 @@
 %sp=serial_port_start('COM4');
 %pioneer_init(sp);
-%pioneer_set_controls(sp,100,0.00001);
+
 pioneer_set_controls(sp,100,0);
 
 tic;
@@ -8,14 +8,54 @@ tStart=tic;
 points=[];
 iter=1;
 time=[];
-while t<10
-    if toc(tStart)/2.5>=iter
-        t=toc(tStart)
-        a=pioneer_rad_sonars();
-        points=[points;a(8)];
-        time=[time;t]
-        iter=iter+1;
+t=0;
+while t<70
+    if toc(tStart)/4>=iter
+         t=toc(tStart)
+         a=pioneer_read_sonars();
+         points=[points;a(1)*10^-3];
+         time=[time;t]
+         iter=iter+1;
+
+    end
+    if (points(length(points))- points(length(points)-1))>0.15
+        break;
     end
 end
-fuc=polyval(time,points,1);
-w=polyder(p)
+[f, ~] = polyfit(time, points, 1);
+angle = atan( f(1) + f(2) );
+angle = rad2deg(angle);
+v=angle/time(length(time));
+
+tStart=tic;
+pioneer_set_controls(sp,100,-round(v));
+
+if toc(tStart)>1
+    pioneer_set_controls(sp,100,0);
+end
+
+tic;
+tStart=tic;
+points=[];
+iter=1;
+time=[];
+t=0;
+while t<70
+    if toc(tStart)/4>=iter
+         t=toc(tStart)
+         a=pioneer_read_sonars();
+         points=[points;a(1)*10^-3];
+         time=[time;t]
+         iter=iter+1;
+
+    end
+    if length(points)>2
+        if (points(length(points))- points(length(points)-1))>0.15
+            break;
+        end
+    end
+end
+[f, ~] = polyfit(time, points, 1);
+angle = atan( f(1) + f(2) );
+angle = rad2deg(angle);
+v=angle/time(length(time));
