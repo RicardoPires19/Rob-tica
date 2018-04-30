ec68d920534585a7c189fad3060efd6a7023fd6e
leonorsilva <leonor.oliveira.e.silva@tecnico.ulisboa.pt>
2018-04-30 11:12:39 +0100

    Add files via upload

--- lab2.m
+++ lab2.m
@@ -2,60 +2,46 @@
 %pioneer_init(sp);
 
 pioneer_set_controls(sp,100,0);
-
-tic;
-tStart=tic;
-points=[];
-iter=1;
-time=[];
-t=0;
-while t<70
-    if toc(tStart)/4>=iter
-         t=toc(tStart)
-         a=pioneer_read_sonars();
-         points=[points;a(1)*10^-3];
-         time=[time;t]
-         iter=iter+1;
-
-    end
-    if (points(length(points))- points(length(points)-1))>0.15
-        break;
-    end
-end
-[f, ~] = polyfit(time, points, 1);
-angle = atan( f(1) + f(2) );
-angle = rad2deg(angle);
-v=angle/time(length(time));
-
-tStart=tic;
-pioneer_set_controls(sp,100,-round(v));
-
-if toc(tStart)>1
-    pioneer_set_controls(sp,100,0);
-end
-
-tic;
-tStart=tic;
-points=[];
-iter=1;
-time=[];
-t=0;
-while t<70
-    if toc(tStart)/4>=iter
-         t=toc(tStart)
-         a=pioneer_read_sonars();
-         points=[points;a(1)*10^-3];
-         time=[time;t]
-         iter=iter+1;
-
-    end
-    if length(points)>2
-        if (points(length(points))- points(length(points)-1))>0.15
+a=pioneer_read_sonars();
+while 1
+    tic;
+    tStart=tic;
+    points=[];
+    iter=1;
+    time=[];
+    t=0;
+    a(1)
+    diag=280+a(1)+a(8)
+    cor=1650
+    ang=acos(diag/cor)
+    while a(1)*cos(ang)>300 && a(1)*cos(ang)<846
+        if toc(tStart)/7>=iter
+             t=toc(tStart)
+             a=pioneer_read_sonars();
+             a(1)
+             points=[points;a(1)*10^-3];
+             time=[time;t]
+             iter=iter+1;
+             pause(0.2)
+        end
+       
+        if length(points)>2 && (points(length(points))- points(length(points)-1))>0.15
+            fprintf('break');
             break;
         end
     end
+    time
+    if length(time)>0
+        [f, ~] = polyfit(time, points, 1);
+        angle = atan( f(1) + f(2) );
+        angle = rad2deg(angle);
+        v=angle/time(length(time));
+    end
+    tStart=tic;
+    pioneer_set_controls(sp,100,-round(v));
+
+    if toc(tStart)>0.1
+        pioneer_set_controls(sp,100,0);
+        fprintf('sidfniasou');
+    end
 end
-[f, ~] = polyfit(time, points, 1);
-angle = atan( f(1) + f(2) );
-angle = rad2deg(angle);
-v=angle/time(length(time));
