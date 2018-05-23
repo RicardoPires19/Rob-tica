7cad54bf780378307a55f3665330eca8fa0df0b0
leonorsilva <leonor.oliveira.e.silva@tecnico.ulisboa.pt>
2018-05-23 13:07:16 +0100

    s
    d

--- curvasdecerto.m
+++ curvasdecerto.m
@@ -12,6 +12,7 @@ value=11500;
 pos=1;
 points=[];
 volta_correcao=true;
+tira_medidas=true;
 ROBOT_VELOCITY_WHILE_FIXING = 200;
 while a(4)>200
     fprintf('begin\n')
@@ -32,6 +33,7 @@ while a(4)>200
     fprintf('fora ciclo com angulo %f var dist esta a %f \n', ang, dist)
     tStart=tic;
     while true
+        if tira_medidas
             t=toc(tStart);
             a=pioneer_read_sonars();
             if length(points)>15
@@ -66,7 +68,7 @@ while a(4)>200
                     break;
                 end
             end
-            
+        end
             while abs(b(pos))>value
                 a=pioneer_read_sonars();
                 b=pioneer_read_odometry();
@@ -103,6 +105,7 @@ while a(4)>200
                         end
                         num_voltas=num_voltas+1;
                     end
+                    tira_medidas=true;
                     a=pioneer_read_sonars();
                     d=a(1)^2+a(2)^2-2*a(1)*a(2)*cos(0.7);
                     c=(a(2)^2+a(1)^2-d)/(2*sqrt(d)*a(1));
@@ -143,24 +146,27 @@ while a(4)>200
             while dist<400 || dist>430
                 volta_correcao=false;
                 b=pioneer_read_odometry();
+                pause(0.2)
                 if abs(b(pos))>value-500
+                    tira_medidas=false;
                     break;
                 end
                 fprintf('dist %d \n', dist)
                 a=pioneer_read_sonars();
+                pause(0.2)
                 d=a(1)^2+a(2)^2-2*a(1)*a(2)*cos(0.7);
                 c=(a(2)^2+a(1)^2-d)/(2*sqrt(d)*a(1));
                 angle=pi-c;
                 dist=sin(angle)*a(1);
                 if dist<400
                     if dist<300
-                        pioneer_set_controls(sp,ROBOT_VELOCITY_WHILE_FIXING,2*(-ceil(abs(v))));
+                        pioneer_set_controls(sp,ROBOT_VELOCITY_WHILE_FIXING,-abs(v));
                     else
-                        pioneer_set_controls(sp,ROBOT_VELOCITY_WHILE_FIXING,-ceil(abs(v)));    
+                        pioneer_set_controls(sp,ROBOT_VELOCITY_WHILE_FIXING,-abs(v));    
                     end
                 else
                     if dist>500
-                        pioneer_set_controls(sp,ROBOT_VELOCITY_WHILE_FIXING,2*ceil(abs(v)));
+                        pioneer_set_controls(sp,ROBOT_VELOCITY_WHILE_FIXING,abs(v));
                     else
                         pioneer_set_controls(sp,ROBOT_VELOCITY_WHILE_FIXING,ceil(abs(v)));
                     end
@@ -179,9 +185,9 @@ while a(4)>200
             fprintf('SENSORS\n [A(1)] %f\n [A(2)] %f\n [C] %f\n', a(1),a(2),c)
             while (a(1)>4500 && a(2)>4500) || abs(c-0.8727)>0.02
                 if v<0
-                    pioneer_set_controls(sp,ROBOT_VELOCITY_WHILE_FIXING, ceil(abs(v)));                        
+                    pioneer_set_controls(sp,ROBOT_VELOCITY_WHILE_FIXING, abs(v));                        
                 else
-                    pioneer_set_controls(sp,ROBOT_VELOCITY_WHILE_FIXING, -ceil(v));
+                    pioneer_set_controls(sp,ROBOT_VELOCITY_WHILE_FIXING, -abs(v));
                 end
                 fprintf('distancia %d endireitar para centro \n ', dist)
                 a=pioneer_read_sonars();
@@ -197,7 +203,9 @@ while a(4)>200
                 b=pioneer_read_odometry();
                 if abs(b(pos))>value-1000 && abs(c-0.8727)>0.1
                     fprintf('curva durante o endireitar')
+                    tira_medidas=false;
                     break;
+                    
                 end
 
                 fprintf('fim endireitar angulo %f \n', c)
@@ -209,14 +217,14 @@ while a(4)>200
                 dist=sin(angle)*a(1);
                 if dist<250
                     fprintf('endireitar demasiado proximo parede esquerda')
-                    pioneer_set_controls(sp, ROBOT_VELOCITY_WHILE_FIXING, -2);
-                    pause(1);
+                    pioneer_set_controls(sp, ROBOT_VELOCITY_WHILE_FIXING, -6);
+                    pause(0.25);
                     volta_correcao=true;
                     break;
-                elseif dist>500
+                elseif dist>480
                     fprintf('endireitar demasiado proximo parede direita')
-                    pioneer_set_controls(sp, ROBOT_VELOCITY_WHILE_FIXING, 2);
-                    pause(1);
+                    pioneer_set_controls(sp, ROBOT_VELOCITY_WHILE_FIXING, 6);
+                    pause(0.25);
                     volta_correcao=true;
                     break;
                 end
